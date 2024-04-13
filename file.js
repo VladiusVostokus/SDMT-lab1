1963326bba07dcd4cf659af354d6924ef5a8dea5
VladiusVostokus <kovalenkov222@gmail.com>
2024-04-13 20:19:46 +0300

    Handle errors
    Add try/catch around reading and writing to handle an errors.

--- file.js
+++ file.js
@@ -4,13 +4,22 @@ const fs = require('fs');
 
 const consoleArgs = process.argv;
 const input = consoleArgs[2];
-const buffer = fs.readFileSync(input,'utf-8');
-const text = buffer.toString();
+let buffer, text;
+try {
+    buffer = fs.readFileSync(input,'utf-8');
+    text = buffer.toString();
+} catch(err){
+    console.error("invalid markdown", err)
+}
 
 if (consoleArgs.includes('--out')) {
+    try {
     const outIndex = consoleArgs.indexOf('--out')
     const output = consoleArgs[outIndex + 1]
     fs.writeFileSync(output, text);
+    } catch(err) {
+        console.error("invalid html", err)
+    }
 }
 else {
     console.log(text);
