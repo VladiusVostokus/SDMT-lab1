61b9e4cae6de4a202caf5caadf0af569be5b86c8
VladiusVostokus <kovalenkov222@gmail.com>
2024-04-13 11:40:50 +0300

    Initial flags logic
    Add flags simple flags logic to handle console input. Now program can
    read .txt file from console argument and write to the .html file if there is
    '--out' flag or write text to console, if no flag '--out'.

--- file.js
+++ file.js
@@ -1,12 +1,17 @@
 'use strict';
 
-//const fs = require('fs');
+const fs = require('fs');
 
 const consoleArgs = process.argv;
-console.log(consoleArgs)
+const input = consoleArgs[2];
+const buffer = fs.readFileSync(input,'utf-8');
+const text = buffer.toString();
 
-//const buffer = fs.readFileSync('input.txt','utf-8');
-//const text = buffer.toString();
-
-//fs.writeFileSync('output.html', text);
-//console.log(text);
+if (consoleArgs.includes('--out')) {
+    const outIndex = consoleArgs.indexOf('--out')
+    const output = consoleArgs[outIndex + 1]
+    fs.writeFileSync('output.html', text);
+}
+else {
+    console.log(text);
+}
