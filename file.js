15cb3c7bbed927301cc1e9816a09781f604c9dc5
VladiusVostokus <kovalenkov222@gmail.com>
2024-04-20 13:57:30 +0300

    Create file of converter
    Create a toHTMLConverter file and import it to main file.
    Now it just return the text which was passed as an argument, but in the
    future here will be the logic of converting symbols from markdown to html.

--- file.js
+++ file.js
@@ -1,13 +1,15 @@
 'use strict';
 
 const fs = require('fs');
+const toHTMLConverter = require('./toHTMLConverter.js');
 
 const consoleArgs = process.argv;
 const input = consoleArgs[2];
-let buffer, text;
+let buffer, text, result;
 try {
     buffer = fs.readFileSync(input,'utf-8');
     text = buffer.toString();
+    result = toHTMLConverter(text);
 } catch(err){
     console.error("invalid markdown", err)
 }
@@ -16,11 +18,11 @@ if (consoleArgs.includes('--out')) {
     try {
     const outIndex = consoleArgs.indexOf('--out')
     const output = consoleArgs[outIndex + 1]
-    fs.writeFileSync(output, text);
+    fs.writeFileSync(output, result);
     } catch(err) {
         console.error("invalid html", err)
     }
 }
 else {
-    console.log(text);
+    console.log(result);
 }
---
+++ toHTMLConverter.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const toHTMLConverter = (text) => {
+    return text;
+}
+
+module.exports = toHTMLConverter;
