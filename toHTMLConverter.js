6807b6bf6df62f2ee67697686c014484a82f70bb
VladiusVostokus <kovalenkov222@gmail.com>
2024-04-21 14:39:42 +0300

    Add first replacing of bold text
    Used replace() to replace ** in <b> at the start of string, which was
    finded with regexp and replace ** in </b> at the end of string. I tried
    to find way of replacement and this first solution I found, but this
    replace only 1 matching, so I'll try to improve it.

--- toHTMLConverter.js
+++ toHTMLConverter.js
@@ -1,7 +1,17 @@
 'use strict';
 
 const toHTMLConverter = (text) => {
-    return text;
+    const formed = bold(text)
+    return formed;
+}
+
+const bold = (text) => {
+    const boldRegExp = /^\*\*.*\*\*$/;
+    const matchedInfo = text.match(boldRegExp);
+    const matchedText = matchedInfo[0];
+    const replacedStart = matchedText.replace('**','<b>');
+    const replacedEnd = replacedStart.replace('**','</b>')
+    return replacedEnd;
 }
 
 module.exports = toHTMLConverter;
