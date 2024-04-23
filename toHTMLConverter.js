ee30661b1323d0d648a944d5e70ca5554cd3cc4d
VladiusVostokus <kovalenkov222@gmail.com>
2024-04-23 19:20:38 +0300

    Now function can replace all ** with html
    Here was changed RexExp in function bold(and changet it name to
    replaceBold), because precious RegExp can only find whole text, which starts and ends with **.
    Also added function to replace all parts with ** in text.

--- toHTMLConverter.js
+++ toHTMLConverter.js
@@ -1,17 +1,29 @@
 'use strict';
 
 const toHTMLConverter = (text) => {
-    const formed = bold(text)
+    const formed = replaceAllBold(text);
     return formed;
-}
+};
 
-const bold = (text) => {
-    const boldRegExp = /^\*\*.*\*\*$/;
+const replaceBold = (text) => {
+    const boldRegExp = /\w*\*\*.*\*\*$/; 
     const matchedInfo = text.match(boldRegExp);
-    const matchedText = matchedInfo[0];
-    const replacedStart = matchedText.replace('**','<b>');
-    const replacedEnd = replacedStart.replace('**','</b>')
-    return replacedEnd;
-}
+    if (matchedInfo !== null) {
+        const replacedStart = text.replace('**','<b>');
+        const replacedEnd = replacedStart.replace('**','</b>');
+        return replacedEnd;
+    }
+    return null;
+};
+
+
+const replaceAllBold = (text) => {
+    let resultText = text;
+    while (true) {
+        const boldRes = replaceBold(resultText);
+        if(boldRes === null) return resultText;
+        resultText = boldRes;
+    }
+};
 
 module.exports = toHTMLConverter;
