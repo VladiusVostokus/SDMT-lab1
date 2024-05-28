cf9491654d37d174e2ec4178808cb47754b710e8
VladiusVostokus <kovalenkov222@gmail.com>
2024-05-28 12:34:55 +0300

    Initial setup of ESlint
    I have set up ESlint and fix some linter notes.

--- toHTMLConverter.js
+++ toHTMLConverter.js
@@ -17,10 +17,10 @@ const errMDCheck = (text) => {
     if (/^[A-Za-z0-9].*_$/.test(text)) {
         throw new Error('Invalid markup: part of text endss with _ but not begins with it');
     }
-    if (/^[\*\*].*[^\*\*]$/.test(text)) {
+    if (/^[**].*[^**]$/.test(text)) {
         throw new Error('Invalid markup: part of text beings with ** but not ends with it');
     }
-    if (/^[A-Za-z0-9].*[\*\*]$/.test(text)) {
+    if (/^[A-Za-z0-9].*[**]$/.test(text)) {
         throw new Error('Invalid markup: part of text endss with ** but not begins with it');
     } 
 };
