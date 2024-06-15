bff47b160de9dd731f5d16720a55c7bacabd1369
VladiusVostokus <kovalenkov222@gmail.com>
2024-06-15 16:33:47 +0300

    Add simple cases of all markup
    Add simple cases just to push to repo and get green results in CI.

--- toHTMLConverter.test.js
+++ toHTMLConverter.test.js
@@ -1,11 +1,19 @@
 'use strict'; 
 
+const toHTMLConverter = require('./toHTMLConverter.js');
+
 const tests = [
-    { description: "some test", value: "a", expected: "a"},
+    { description: "Simple bold", value: "**aaa**", expected: "<p><b>aaa</b></p>"},
+
+    { description: "Simple italic", value: "_aaa_", expected: "<p><i>aaa</i></p>"},
+
+    { description: "Simple monospaced", value: "`aaa`", expected: "<p><tt>aaa</tt></p>"},
+
+    { description: "Simple preformatted", value:"```\naaa\n```", expected: "<p><pre>\naaa\n</pre></p>"},
 ];
 
 for (const t of tests) {
     test(t.description, () => {
-        expect(t.value).toBe(t.expected);
+        expect(toHTMLConverter(t.value)).toBe(t.expected);
     });
 }
