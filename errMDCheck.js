51250a0f2f46f62baa1fe24e3faddf3cb7082cae
VladiusVostokus <kovalenkov222@gmail.com>
2024-06-15 14:53:17 +0300

    Fix italic case in error checking
    If give a string with whitespace on the sides, function will not work
    correctly in intalic cases, so I add trim() to delete spaces.

--- errMDCheck.js
+++ errMDCheck.js
@@ -8,10 +8,10 @@ const errMDCheck = (text) => {
     if (/``` *[^\s]| *[^\s] *```/.test(text)) { 
         return new Error('Invalid markup: incorrect preformatting');
     }
-    if (/^_.*[^_]$/.test(text)) {
+    if (/^_.*[^_]$/.test(text.trim())) {
         return new Error('Invalid markup: part of text beings with _ but not ends with it');
     }
-    if (/^[A-Za-z0-9].*_$/.test(text)) {
+    if (/^[A-Za-z0-9].*_$/.test(text.trim())) {
         return new Error('Invalid markup: part of text endss with _ but not begins with it');
     }
     if (/^[**].*[^**]$/.test(text)) {
