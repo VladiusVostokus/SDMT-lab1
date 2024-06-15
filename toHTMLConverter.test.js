667dbb1a0ca606fcae175ed36a59f980ec36b6e4
VladiusVostokus <kovalenkov222@gmail.com>
2024-06-15 18:04:50 +0300

    Add more complicated example test
    Add bigger and complecated exapmle to show all markups together.

--- toHTMLConverter.test.js
+++ toHTMLConverter.test.js
@@ -10,6 +10,22 @@ const tests = [
     { description: "Simple monospaced", value: "`aaa`", expected: "<p><tt>aaa</tt></p>"},
 
     { description: "Simple preformatted", value:"```\naaa\n```", expected: "<p><pre>\naaa\n</pre></p>"},
+
+    { description: "Complicated", 
+        value:"```\nSome are born great\n\n**dada**\n\n```\nвфівфі\nвфівіф\n**dasda**\n\nsome achieve , `dasdas`\nand have _greatness_ thrust them.", 
+        expected:
+`<p><pre>
+Some are born great
+
+**dada**
+
+</pre>
+вфівфі
+вфівіф
+<b>dasda</b></p>
+<p>some achieve , <tt>dasdas</tt>
+and have <i>greatness</i> thrust them.</p>`
+    },
 ];
 
 for (const t of tests) {
